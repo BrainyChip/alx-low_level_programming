a76ea5be14db40a125f4e0e591aba1d1de4a0a69
Adekunle Joshua <joshuaadekunle249@gmail.com>
2023-06-15 19:17:33 -0700

    101-quote.c - printing without puts anf printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistdio.h>
+/**
+ * main - printing without puts and printf
+ *
+ * Return: always (1)
+ *
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n", 59);
+	return (1);
+}
