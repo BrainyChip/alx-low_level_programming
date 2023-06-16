84995d73ea9bfa8ead29c8d5622e893581651fce
Adekunle Joshua <joshuaadekunle249@gmail.com>
2023-06-15 18:19:46 -0700

    4-puts.c C file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints out output using puts
+ *
+ * Return - each time (0)
+ *
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
