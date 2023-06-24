2e3a0db88871e1d39c55c63fc53a0fb76d74481e
Adekunle Joshua <joshuaadekunle249@gmail.com>
2023-06-23 21:52:48 -0700

    0-isupper.c check for uppercase alphabets

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * isupper - find out uppercase alphabets
+ *
+ * @c: input character
+ *
+ * Return: (1) for true (0) false
+ *
+ *
+ */
+int_isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+		}
+}
+
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ This is the task projects on the  0x04-more_functions_nested_loops ALX
