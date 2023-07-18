3ccb2401fc2622c9c142cdfb7cdc402e152fcea6
Adekunle Joshua <joshuaadekunle249@gmail.com>
2023-07-17 20:12:03 -0700

    6-abs.c - a function that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - function that optimizes value of a int
+ *@r: the value
+ * Description: The standard library provides a similar function.
+ *
+ * Return: (0)
+ *
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+
+		return (r);
+	else
+		return (r * -1);
+}
+
